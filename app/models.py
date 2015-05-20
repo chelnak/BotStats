ca786129a1898e1a2e5470ab62c212a18a17485e
root <craiggumbley@gmail.com>
2015-05-20 16:08:32 +0100

    Updates to database functionality

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+import datetime
 
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,15 +7,15 @@ class Log(db.Model):
     comment_author = db.Column(db.String(50))
     link_id = db.Column(db.String(10))
     replied = db.Column(db.Integer)
-    created_on = db.Column(db.DateTime)
+    fact_id = db.Column(db.Integer)
+    created_on = db.Column(db.DateTime, default=db.func.now())
 
     def __repr__(self):
         return '<Comment Id: %r>' % (self.comment_id)
-"""
+
 class Fact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    fact =  db.Column(db.String(140))
+    fact =  db.Column(db.String(250))
 
     def __repr__(self):
         return '<Fact Id: %r>' % (self.id)
-"""
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from app import app, db
-from .models import Log
-
+from .models import Log, Fact
 
 @app.route('/')
 @app.route('/index')
@@ -11,9 +10,9 @@ def index():
     totalProcessed = Log.query.count()
     repliedTo = Log.query.filter_by(replied=1)
     repliedToCount = Log.query.filter_by(replied=1).count()
-
-
     
+    totalFacts = Fact.query.count()
+
     if repliedTo is None:
         repliedTo = "No records to return"
 
@@ -22,4 +21,5 @@ def index():
                             title='Home',
                             repliedTo = repliedTo,
                             repliedToCount = repliedToCount,
-                            totalProcessed = totalProcessed)
+                            totalProcessed = totalProcessed,
+                            totalFacts = totalFacts)
